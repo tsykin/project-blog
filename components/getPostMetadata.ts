ebb99c46ac6ef440884f83631282ef035b59e8be
tsykin <tsikinsasha@gmail.com>
2023-08-19 16:25:00 +0200

    added import to getPostMetadata

--- components/getPostMetadata.ts
+++ components/getPostMetadata.ts
@@ -1,12 +1,13 @@
 import fs from "fs";
 import matter from "gray-matter";
+import { PostMetadata } from "../components/PostMetadata";
 
-const getPostMetaData = (): PostMetadata[] => {
+const getPostMetadata = (): PostMetadata[] => {
   const folder = "posts/";
   const files = fs.readdirSync(folder);
   const markdownPosts = files.filter((file) => file.endsWith(".md"));
 
-  // Get gray-matter data from each file
+  // Get gray-matter data from each file.
   const posts = markdownPosts.map((fileName) => {
     const fileContents = fs.readFileSync(`posts/${fileName}`, "utf8");
     const matterResult = matter(fileContents);
@@ -21,4 +22,4 @@ const getPostMetaData = (): PostMetadata[] => {
   return posts;
 };
 
-export default getPostMetaData;
+export default getPostMetadata;
