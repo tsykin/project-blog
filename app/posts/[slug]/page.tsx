420e12fcdfb8f03d57e1f771453550c43e3f3ee3
Aliaksandr Tsykin <tsikinsasha@gmail.com>
2023-08-19 15:44:41 +0200

    init commit

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,38 @@
+import fs from "fs";
+import Markdown from "markdown-to-jsx";
+import matter from "gray-matter";
+import getPostMetaData from "@/components/getPostMetadata";
+
+const getPostContent = (slug: string) => {
+  const folder = "posts/";
+  const file = `${folder}${slug}.md`;
+  const content = fs.readFileSync(file, "utf8");
+  const matterResult = matter(content);
+  return matterResult;
+};
+
+export const generateStaticParams = async () => {
+  const posts = getPostMetaData();
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+};
+
+const PostPage = (props: any) => {
+  const slug = props.params.slug;
+  const post = getPostContent(slug);
+
+  return (
+    <div>
+      <h1 className="text-2xl text-violet-600 text-center">
+        {post.data.title}
+      </h1>
+      <h3 className="text-center">{post.data.date}</h3>
+      <article className="prose lg:prose-xl">
+        <Markdown>{post.content}</Markdown>
+      </article>
+    </div>
+  );
+};
+
+export default PostPage;
---
+++ components/PostPreview.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { PostMetadata } from "./PostMetadata";
+
+const PostPreview = (props: PostMetadata) => {
+  return (
+    <div className="border border-slate-200 p-4 rounded-md shadow-md bg-white">
+      <Link href={`/posts/${props.slug}`}>
+        <h2 className="font-bold text-biolet-600 hover:underline">
+          {props.title}
+        </h2>
+      </Link>
+      <p className="text-sm text-slate-400">{props.date}</p>
+      <p className="text-slate-700">{props.subtitle}</p>
+    </div>
+  );
+};
+
+export default PostPreview;
